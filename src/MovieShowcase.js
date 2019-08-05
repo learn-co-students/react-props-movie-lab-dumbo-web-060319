1553e6087d0808c6f084d79f87268a11e7cf11a5
Brandon <brandon.lau86@gmail.com>
2019-08-05 18:05:19 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
 
-export default class MovieShowcase extends Component {
-
+export default class MovieShowcase extends Component 
+{
   generateMovieCards = () => {
     // map over your movieData array and return the correct 
+    let all = movieData.map((stuff) =>
+      <MovieCard title = {stuff.title} IMDBRating = {stuff.IMDBRating} genres = {stuff.genres} poster = {stuff.poster}/>
+    )
+    return all;
   }
 
   render() {
--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -12,16 +12,25 @@ export default class CardBack extends Component {
 
   generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
+    let x = this.props.IMDBRating;
+    if (x !== null)
+    {
+      return <img src = {imgMapper[x]} />
+    }
+    else 
+    {
+      return <h4>No Rating Found</h4>
+    }
   }
 
   render() {
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title">{this.props.title}</h3>
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        { this.generateRatingElement()/* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
         <span />
-        <h5 className="genres"></h5>
+        <h5 className="genres">{this.props.genres.join(", ")}</h5>
       </div>
     )
   }
